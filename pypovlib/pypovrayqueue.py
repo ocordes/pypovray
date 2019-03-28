ff120f2a503610691480c8a266063014ab7517d6
Oliver Cordes <ocordes@astro.uni-bonn.de>
2019-03-28 18:42:40 +0100

    - work on image submit

--- pypovlib/pypovrayqueue.py
+++ pypovlib/pypovrayqueue.py
@@ -3,18 +3,20 @@
 pypovlib/pypovrayqueue.py
 
 written by: Oliver Cordes 2019-03-04
-changed by: Oliver Cordes 2019-03-05
+changed by: Oliver Cordes 2019-03-18
 
 """
 
 import sys,os
 
+import configparser
 import tarfile
 import uuid
 
 try:
     from client.api import Session
     from client.projects import Project
+    from client.images import Image
 except:
     print('rayqueue client modules not found!')
     os.exit(-1)
@@ -39,21 +41,26 @@ class RQPovFile(PovFile):
                         verbose=False,
                         camera_optimize=False,
                         config=None,
-                        rq_project_name=None):
+                        rq_project_name=None,
+                        width=640,
+                        height=480):
         PovFile.__init__(self,filename=filename,
                             verbose=verbose,
                             camera_optimize=camera_optimize)
 
-        self._session = Session(config=config)
+        self._session = Session(config=config, verbose=True)
 
 
         self._rq_project_name = rq_project_name
         self._rq_projects = None
         self._rq_project  = None
 
+        self._width = width
+        self._height = height
+
 
     def _load_projects(self):
-        self._rq_projects = Project.query(self._session)
+        self._rq_projects = Project.queryall(self._session)
 
 
     def _select_rq_project(self):
@@ -66,7 +73,7 @@ class RQPovFile(PovFile):
             user_input = input('Enter the project id:')
             print()
             try:
-                id = int(user_input)
+                id = int(user_input)-1   # count from zero!
                 return self._rq_projects[id]
             except:
                 pass
@@ -84,6 +91,25 @@ class RQPovFile(PovFile):
                 tar.add(f, filter=tarinfo_reset)
 
 
+    def _create_master_ini(self):
+
+        pre, ext = os.path.splitext(self._filename)
+        outname = pre + '.png'
+
+        data = { 'scene': self._filename,
+                 'width': self._width,
+                 'height': self._height,
+                 'outfile': outname }
+
+        config = configparser.ConfigParser()
+        config['DEFAULT'] = data
+        filename = 'scene.ini'
+        with open(filename, 'w') as configfile:
+            config.write(configfile)
+
+        return filename
+
+
     def write_povfile(self, filename=None):
         # first save the standard file
         PovFile.write_povfile(self, filename=filename)
@@ -91,10 +117,13 @@ class RQPovFile(PovFile):
 
         # generates a temptile name
 
-        tempfile = os.path.join('/tmp','image_%s.tar.gz' %(str(uuid.uuid4())))
+        #tempfile = os.path.join('/tmp','image_%s.tar.gz' %(str(uuid.uuid4())))
+        tempfile = os.path.join('/tmp','image_%s.tar.gz' %('simple'))
 
         listoffiles = []
         listoffiles.append(self._filename)
+        listoffiles.append(self._create_master_ini())
+
         self._create_rq_image_file(tempfile, listoffiles)
 
         # now connects to the RQ service
@@ -105,4 +134,49 @@ class RQPovFile(PovFile):
 
         print('Successfully logged in to the RQ service!')
         self._load_projects()
-        self._rq_project_id = self._select_rq_project()
+        self._rq_project = self._select_rq_project()
+
+
+        # clear old files...
+        ret = self._rq_project.clear_images(self._session)
+        if ret:
+            print('All old files cleared!')
+        else:
+            print('Something went wrong while clearing old files!')
+
+        # upload the image description
+
+        do_trying = True
+        while do_trying:
+            image_id = Image.create(self._session, self._rq_project.id, tempfile)
+
+            if image_id != -1:
+                do_trying = False
+            else:
+                print('Image creation failed! Possible solutions:')
+                print('------------------------------------------')
+                print(' <1> Reset project')
+                user_input = int(input('Your choice: '))
+
+                if user_input != 1:
+                    do_trying = False
+                else:
+                    self._rq_project.reset(self._session)
+
+        # end while
+
+
+        # remove temporary file
+        os.remove(tempfile)
+
+        if image_id != -1:
+            print('New image with id=%i created' % image_id)
+
+            # this is the code for testing the loop
+            # waiting for the image to be ready!
+            image = Image.query(self._session, image_id)
+
+            print(image.state)
+            print(image.status())
+        else:
+            print('Image couldn\'t be created!')
