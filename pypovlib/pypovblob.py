058d16ba061e33cd520fd866a23d1e6449a8c705
Oliver Cordes <ocordes@astro.uni-bonn.de>
2019-08-03 18:42:04 +0200

    - add some blob structures

---
+++ pypovlib/pypovblob.py
@@ -0,0 +1,26 @@
+#
+# pypovlib/pypovblob.py
+#
+# written by: Oliver Cordes 2019-08-03
+# changed by: Oliver Cordes 2019-08-03
+#
+#
+
+from pypovlib import *              # thought this should be enough
+from pypovlib.pypovanimation import *
+from pypovlib.pypovweather import *
+from pypovlib.pypovobjects import *
+from pypovlib.pypovtextures import *
+from pypovlib.pypovlights import *
+from pypovlib.pypovtools import *
+from pypovlib.pypovcamera import *
+
+
+
+class PovBlob(PovCSGObject):
+    pass
+
+
+
+class PovBlobSphere(PovCSGSphere):
+    pass
